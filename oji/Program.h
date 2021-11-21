4bb96a3625edb341d4ef746ef963628f0f1dbeda
amreemaru <heartof.effort@gmail.com>
2021-11-22 02:04:17 +0700

    update

--- oji/Program.h
+++ oji/Program.h
@@ -7,32 +7,16 @@
 #include <conio.h>
 #include "ScoreList.h"
 #include "functions.h"
-
+#include "Textbaseconsole.h"
 using namespace std;
 
 //setcosole
 #define screen_x 60
 #define screen_y 25
+#define lenght 100
 const int width = 30;
 const int height = 25;
-#define lenght 100
 const int wx = 30, hy = 25;
-
-HANDLE wHnd;
-COORD bufferSize = { screen_x,screen_y };
-SMALL_RECT windowSize = { 0,0,screen_x - 1,screen_y - 1 };
-COORD characterPos = { 0,0 };
-
-//double buffer
-CHAR_INFO consoleBuffer[screen_x * screen_y];
-
-//event buffers
-HANDLE rHnd;
-DWORD fdwMode;
-DWORD numEvents = 0;
-DWORD numEventsRead = 0;
-
-//function
 
 //food
 #define count 100
@@ -274,59 +258,6 @@ void control_setting()
 		}
 		delete[] eventBuffer;
 	}
-}
-
-void setcursor(bool visible)
-{
-	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO lpCursor;
-	lpCursor.bVisible = visible;
-	lpCursor.dwSize = 20;
-	SetConsoleCursorInfo(console, &lpCursor);
-}
-
-int setConsole(int x, int y)
-{
-	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
-	SetConsoleScreenBufferSize(wHnd, bufferSize);
-	return 0;
-}
-
-void fill_buffer_to_console()
-{
-	WriteConsoleOutputA(wHnd, consoleBuffer, bufferSize, characterPos, &windowSize);
-}
-
-void clear_buffer()
-{
-	for (int y = 0; y < screen_y; ++y) {
-		for (int x = 0; x < screen_x; ++x) {
-			consoleBuffer[x + screen_x * y].Char.AsciiChar = ' ';
-			consoleBuffer[x + screen_x * y].Attributes = 0;
-		}
-	}
-}
-
-int setMode()
-{
-	rHnd = GetStdHandle(STD_INPUT_HANDLE);
-	fdwMode = ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT |
-		ENABLE_MOUSE_INPUT;
-	SetConsoleMode(rHnd, fdwMode);
-	return 0;
-}
-
-void gotoxy(int x, int y)
-{
-	COORD c = { x, y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-
-void setcolor(int fg, int bg)
-{
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
 }
 
 void fill_food()
@@ -1042,10 +973,10 @@ void boardpage()
 	{
 		clear_buffer();
 		fill_buffer_to_console();
-		gotoxy(0, 2);
+		gotoxy(0, 3);
 		setcolor(6 ,0);
-		cout << "=========================LEADERBOARD========================";
-		gotoxy(0, 4);
+		cout << " ========================LEADERBOARD========================";
+		gotoxy(0, 5);
 		setcolor(10, 0);
 		if (challengeboard == true)
 		{
@@ -1062,7 +993,7 @@ void boardpage()
 			score_list.saveFile();
 		}
 		setcolor(6, 0);
-		cout << endl << "============================================================";
+		cout << endl << " ===========================================================";
 		gotoxy(29, 24);
 		cout << "PRESS ENTER : BACK TO MAIN MENU";
 		runround++;
---
+++ oji/Textbaseconsole.h
@@ -0,0 +1,69 @@
+#pragma once
+#define screen_x 60
+#define screen_y 25
+HANDLE wHnd;
+COORD bufferSize = { screen_x,screen_y };
+SMALL_RECT windowSize = { 0,0,screen_x - 1,screen_y - 1 };
+COORD characterPos = { 0,0 };
+
+//double buffer
+CHAR_INFO consoleBuffer[screen_x * screen_y];
+
+//event buffers
+HANDLE rHnd;
+DWORD fdwMode;
+DWORD numEvents = 0;
+DWORD numEventsRead = 0;
+
+void setcursor(bool visible)
+{
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO lpCursor;
+	lpCursor.bVisible = visible;
+	lpCursor.dwSize = 20;
+	SetConsoleCursorInfo(console, &lpCursor);
+}
+
+int setConsole(int x, int y)
+{
+	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
+	SetConsoleScreenBufferSize(wHnd, bufferSize);
+	return 0;
+}
+
+void fill_buffer_to_console()
+{
+	WriteConsoleOutputA(wHnd, consoleBuffer, bufferSize, characterPos, &windowSize);
+}
+
+void clear_buffer()
+{
+	for (int y = 0; y < screen_y; ++y) {
+		for (int x = 0; x < screen_x; ++x) {
+			consoleBuffer[x + screen_x * y].Char.AsciiChar = ' ';
+			consoleBuffer[x + screen_x * y].Attributes = 0;
+		}
+	}
+}
+
+int setMode()
+{
+	rHnd = GetStdHandle(STD_INPUT_HANDLE);
+	fdwMode = ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT |
+		ENABLE_MOUSE_INPUT;
+	SetConsoleMode(rHnd, fdwMode);
+	return 0;
+}
+
+void gotoxy(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void setcolor(int fg, int bg)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+}
